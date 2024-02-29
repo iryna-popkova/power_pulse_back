a14c51827603eebd8af326c83e1684c958d9a9f8
iryna-popkova <131870449+iryna-popkova@users.noreply.github.com>
2024-02-29 12:39:07 +0200

    errors update

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRouter = require("./routes/authRouter.js");
 
 dotenv.config();
 const { DB_HOST } = process.env;
