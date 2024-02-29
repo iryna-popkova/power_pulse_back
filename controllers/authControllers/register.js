8f31c05b84e607204baf2b2d019c37c9a8b3978e
iryna-popkova <131870449+iryna-popkova@users.noreply.github.com>
2024-02-29 13:58:46 +0200

    User fix

--- controllers/authControllers/register.js
+++ controllers/authControllers/register.js
@@ -1,4 +1,4 @@
-const { User } = require("../models/user");
+const { User } = require("../../models/user");
 const { HttpError } = require("../../helpers");
 const bcrypt = require("bcrypt");
 const gravatar = require("gravatar");
