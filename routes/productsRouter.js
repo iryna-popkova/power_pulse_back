100a0a370c0319723002bff5bc91586786a0fe91
iryna-popkova <131870449+iryna-popkova@users.noreply.github.com>
2024-02-29 14:42:36 +0200

    routes

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -5,6 +5,7 @@ const { ctrlWrapper } = require("../helpers/index.js");
 const { validateBody, authenticate } = require("../midleware");
 
 const { Product } = require("../models/productsModel.js");
+const { ProductsCategory } = require("../models/categoriesModel");
 
 const productsRouter = express.Router();
 
