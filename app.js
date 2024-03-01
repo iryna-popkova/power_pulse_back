30306888eb7f05a0d94a7828bec406e0c29ef93e
iryna-popkova <131870449+iryna-popkova@users.noreply.github.com>
2024-03-01 13:55:59 +0200

    swagger

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/users", authRouter);
 app.use("/diary", diaryRouter);
 app.use("/products", productsRouter);
