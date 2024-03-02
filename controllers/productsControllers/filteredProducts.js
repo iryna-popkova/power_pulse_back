22452dbbd60fb4f83f2b9dcd6a53ff0c761827a8
iryna-popkova <131870449+iryna-popkova@users.noreply.github.com>
2024-03-02 09:42:12 +0200

    products

--- controllers/productsControllers/filteredProducts.js
+++ controllers/productsControllers/filteredProducts.js
@@ -2,11 +2,44 @@ const { Product } = require("../../models");
 const { HttpError } = require("../../helpers");
 
 const filteredProducts = async (req, res) => {
-  const result = await Product.find();
-  if (!result) {
-    throw HttpError(404, "Not found");
+  const { category, search, recommended } = req.query;
+  const where = {};
+
+  if (category) {
+    where.category = category;
   }
-  res.json(result);
+  if (search) {
+    where.title = { $regex: search, $options: "i" };
+  }
+
+  if (recommended) {
+    const { user: id } = req;
+    const user = await User.findById(id);
+    const bloodType = user.bodyParams.blood;
+    if (!bloodType) {
+      throw HttpError(404);
+    }
+
+    if (recommended === "true") {
+      where[`groupBloodNotAllowed.${bloodType}`] = true;
+    } else {
+      where[`groupBloodNotAllowed.${bloodType}`] = false;
+    }
+  }
+  const { page = 1, limit = 100 } = req.query;
+  const total = await Product.find(where).count();
+  const products = await Product.find(where)
+    .limit(limit)
+    .skip(limit * (page - 1));
+  if (!products) {
+    throw HttpError(400);
+  }
+  res.json({
+    total,
+    page,
+    limit,
+    products,
+  });
 };
 
 module.exports = filteredProducts;
