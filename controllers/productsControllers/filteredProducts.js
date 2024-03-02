ab4d649d3da3affcc3125a2f3b3e1b52b19f90a7
iryna-popkova <131870449+iryna-popkova@users.noreply.github.com>
2024-03-02 09:54:48 +0200

    filter products v2

--- controllers/productsControllers/filteredProducts.js
+++ controllers/productsControllers/filteredProducts.js
@@ -1,45 +1,72 @@
 const { Product } = require("../../models");
 const { HttpError } = require("../../helpers");
 
+// const filteredProducts = async (req, res) => {
+//   const { category, search, recommended } = req.query;
+//   const where = {};
+
+//   if (category) {
+//     where.category = category;
+//   }
+//   if (search) {
+//     where.title = { $regex: search, $options: "i" };
+//   }
+
+//   if (recommended) {
+//     const { user: id } = req;
+//     const user = await User.findById(id);
+//     const bloodType = user.bodyParams.blood;
+//     if (!bloodType) {
+//       throw HttpError(404);
+//     }
+
+//     if (recommended === "true") {
+//       where[`groupBloodNotAllowed.${bloodType}`] = true;
+//     } else {
+//       where[`groupBloodNotAllowed.${bloodType}`] = false;
+//     }
+//   }
+//   const { page = 1, limit = 100 } = req.query;
+//   const total = await Product.find(where).count();
+//   const products = await Product.find(where)
+//     .limit(limit)
+//     .skip(limit * (page - 1));
+//   if (!products) {
+//     throw HttpError(400);
+//   }
+//   res.json({
+//     total,
+//     page,
+//     limit,
+//     products,
+//   });
+// };
+
+// module.exports = filteredProducts;
+
 const filteredProducts = async (req, res) => {
-  const { category, search, recommended } = req.query;
-  const where = {};
+  try {
+    const userBloodType = req.user.blood;
 
-  if (category) {
-    where.category = category;
-  }
-  if (search) {
-    where.title = { $regex: search, $options: "i" };
-  }
+    const recommendedProducts = await Product.find({
+      $or: [
+        { [`groupBloodNotAllowed.${userBloodType}`]: { $exists: false } },
+        { [`groupBloodNotAllowed.${userBloodType}`]: false },
+      ],
+    });
 
-  if (recommended) {
-    const { user: id } = req;
-    const user = await User.findById(id);
-    const bloodType = user.bodyParams.blood;
-    if (!bloodType) {
-      throw HttpError(404);
-    }
+    const notRecommendedProducts = await Product.find({
+      [`groupBloodNotAllowed.${userBloodType}`]: true,
+    });
 
-    if (recommended === "true") {
-      where[`groupBloodNotAllowed.${bloodType}`] = true;
-    } else {
-      where[`groupBloodNotAllowed.${bloodType}`] = false;
-    }
+    res.status(200).json({
+      recommendedProducts,
+      notRecommendedProducts,
+    });
+  } catch (error) {
+    console.error("Error retrieving products:", error);
+    res.status(500).json({ message: "Internal Server Error" });
   }
-  const { page = 1, limit = 100 } = req.query;
-  const total = await Product.find(where).count();
-  const products = await Product.find(where)
-    .limit(limit)
-    .skip(limit * (page - 1));
-  if (!products) {
-    throw HttpError(400);
-  }
-  res.json({
-    total,
-    page,
-    limit,
-    products,
-  });
 };
 
-module.exports = filteredProducts;
+module.exports = { filteredProducts };
