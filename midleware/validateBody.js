3e60799eb8d2d9582dd0c5eefb5012d1b94634b1
iryna-popkova <131870449+iryna-popkova@users.noreply.github.com>
2024-03-13 12:19:15 +0200

    Update validateBody.js

--- midleware/validateBody.js
+++ midleware/validateBody.js
@@ -1,12 +1,33 @@
 const { HttpError } = require("../helpers");
 
+// const validateBody = (schema) => {
+//   const func = (req, _, next) => {
+//     const { error } = schema.validate(req.body);
+//     if (error) {
+//       next(HttpError(400), error.message);
+//     }
+//     next();
+//   };
+
+//   return func;
+// };
+
+// module.exports = validateBody;
+
 const validateBody = (schema) => {
-  const func = (req, _, next) => {
+  const func = (req, res, next) => {
     const { error } = schema.validate(req.body);
     if (error) {
-      next(HttpError(400), error.message);
+      // Extract error messages from the Joi validation error object
+      const errorMessage = error.details
+        .map((detail) => detail.message)
+        .join(", ");
+      // Pass error message to HttpError constructor along with status code
+      next(HttpError(400, errorMessage));
+    } else {
+      // If no error, proceed to the next middleware
+      next();
     }
-    next();
   };
 
   return func;
